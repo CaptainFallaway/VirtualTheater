2b8bd0c617447a170cb3923d8a51f2b85766517c
CaptainFallaway <alexander.wollmer97@gmail.com>
2024-08-26 00:51:44 +0200

    Added routers and moved lines

--- server.js
+++ server.js
@@ -1,14 +1,18 @@
-import express from "express"
-import { handler } from "./frontend/build/handler.js"
 import dotenv from "dotenv"
+import express from "express"
+import * as root from "./routes/root.js";
+import * as admin from "./routes/admin.js";
+import { handler } from "./frontend/build/handler.js"
 
 dotenv.config()
-
-const app = express()
 
 const PORT = process.env.PORT || 3000
 const HOST = process.env.HOST || "localhost"
 
+const app = express()
+
+app.use("/admin", admin.router)
+app.use("/", root.router)
 app.use(handler)
 
 app.listen(PORT, HOST, () => {
