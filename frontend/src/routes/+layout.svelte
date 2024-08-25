449607cb0019b26b66b864e6839276f85f0a19e3
CaptainFallaway <alexander.wollmer97@gmail.com>
2024-08-26 00:53:25 +0200

    feat: added darkmode toggle button

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,1 +1,11 @@
-<script>import "../app.css";</script><slot></slot>
+<script>
+    import "../app.css"
+
+    import { toggleMode, ModeWatcher } from "mode-watcher"
+    import { Button } from "$lib/components/ui/button"
+</script>
+
+<ModeWatcher />
+<Button on:click={toggleMode}>D</Button>
+
+<slot></slot>
